8833a6e16462e3c82cdb4d5cb751eb3317216fbf
Bhaskar P <bhaskar.p@s.amity.edu>
2025-03-07 09:58:33 +0530

    updated

--- bh.py
+++ bh.py
@@ -9,3 +9,24 @@ sum = num1 + num2
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
+
+
+n = int (input (“Enter the number for which the factorial needs to be calculated: “)
+
+factorial = 1
+
+if n < 0:
+
+              print (“Factorial cannot be calculated, non-integer input”)
+
+elif n == 0:
+
+              print (“Factorial of the number is 1”)
+
+else:
+
+              for i in range (1, n+1):
+
+                             factorial = factorial *i
+
+              print (“Factorial of the given number is: “, factorial
